4405d583467ac7639fc67ef0b7d48078b6b74caa
stevehndz <steve.r.hndz@gmail.com>
2024-06-14 11:20:04 -0600

    feat(slider): added slider module, added styles

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 import React, { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -17,6 +17,8 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
   {
@@ -56,13 +58,20 @@ const Work = () => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className="min-h-[80vh] flex flex-col justify-center py-12 lg:px-0"
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-4 lg:py-12 lg:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row lg:gap-[30px]">
           <div className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2 lg:order-none">
-            <div className="flex flex-col gap-[30px]">
+            <div className="flex flex-col gap-[15px] lg:gap-[30px] mb-8 lg:mb-0">
               {/* ourline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
@@ -123,11 +132,36 @@ const Work = () => {
               spaceBetween={30}
               slidesPerView={1}
               onSlideChange={handleSlideChange}
-              className="lg:h-[520px] mb-12"
+              className="lg:h-[480px] mb-12 lg:mb-0"
             >
               {projects.map((project, index) => {
-                return <SwiperSlide key={index}>slide</SwiperSlide>;
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[420px] lg:h-[400px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt={project.num}
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
               })}
+              {/* slider buttons */}
+              <WorkSliderBtns
+                containerStyles={
+                  "flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] lg:bottom-0 z-20 w-full justify-between lg:w-max lg:justify-none"
+                }
+                btnStyles={
+                  "bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                }
+              />
             </Swiper>
           </div>
         </div>
